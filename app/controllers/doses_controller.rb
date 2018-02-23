77de98fca142f7c915c40a90dea61b8b03eeffe7
Owen Moss <owenkmoss@gmail.com>
2018-02-23 13:06:24 +0100

    Added update cocktail with ingredients

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -2,15 +2,28 @@ class DosesController < ApplicationController
 
   def new
     @cocktail = Cocktail.find(params[:cocktail_id])
+    @ingredients = Ingredient.all
     @dose = Dose.new
+
   end
 
   def create
+    @ingredient = Ingredient.find(params[:dose][:ingredient])
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
+    @dose.ingredient = @ingredient
     if @dose.save
       redirect_to @dose.cocktail
+    else
+      render :new
+    end
+  end
+
+  def update
+    @dose = Dose.find(params[:cocktail_id])
+    if @dose.update(dose_params)
+    redirect_to redirect_to cocktail_path
     else
       render :new
     end
@@ -26,6 +39,6 @@ class DosesController < ApplicationController
   private
 
   def dose_params
-    params.require(:dose).permit(:cocktail_id, :description, :ingredient_id)
+    params.require(:dose).permit(:description, :amount)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
 
   root to: 'cocktails#index'
   resources :cocktails do
-    resources :doses, only: [:new, :create]
+    resources :doses, only: [:new, :create, :update]
   end
   resources :doses, only: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+#
